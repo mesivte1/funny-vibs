dd5d71b21df0b61b1f18b002d736693a9d143972
Steve Bob <110861909+mesivte1@users.noreply.github.com>
2022-08-16 18:14:08 -0400

    Update c code .c

--- c code .c
+++ c code .c
@@ -7,4 +7,5 @@ int  main()
     printf("now is the time of rock and hop.\n");
     printf("Adding text to the new branch.");
     printf("testing git in bash command line mode");
+    printf("add text on git hub.\n");
 }
