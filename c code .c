be695520e9fe06d7ba7b593cfca2df266989aa28
Steve bob <mesivte1@gmail.com>
2022-08-16 18:08:27 -0400

    another commit

--- c code .c
+++ c code .c
@@ -6,4 +6,5 @@ int  main()
     printf("Hello to the funny world.\n");
     printf("now is the time of rock and hop.\n");
     printf("Adding text to the new branch.");
+    printf("testing git in bash command line mode");
 }
